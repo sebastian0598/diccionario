507d0d61ad576324dad322772e036a8c825588d3
carlosM99-MAX <70307464+carlosM99-MAX@users.noreply.github.com>
2020-09-10 00:44:11 -0500

    Delete resto_funciones.py

--- resto_funciones.py
+++
@@ -1,8 +0,0 @@
-#eliminar
-eliminar = (input("digite el funcionario que desee eliminar: "))
-del diccionario[eliminar]
-#buscar
-busca=input('introduce la busqueda: ')
-print('resultado: ',diccionario.get(busca,'no encontramos nada'))
-#mostrar
-print(diccionario)
