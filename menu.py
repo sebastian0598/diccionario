6678def55884e5fa54ac08c8b43b4c395f1ad151
franj422 <71057527+franj422@users.noreply.github.com>
2020-09-10 00:33:57 -0500

    Add files via upload
    cambios

---
+++ menu.py
@@ -0,0 +1,35 @@
+def menu():
+    print('1. agregar')
+    print('2. Imprimir')
+    print('3. Eliminar')
+    print('4. Consultar')
+    print('5. salir')
+    opcion = input('bienvenidos que desea hacer? ')
+    return opcion
+
+def main():
+    while True:
+        opcion=menu()
+        if opcion == '1':
+            print('')
+            print()
+
+
+        
+        elif opcion == '2':
+            print('')
+            print()
+
+            
+        elif opcion == '3':
+            print('')
+            print()
+
+        elif opcion == '4':
+            print('')
+            print()
+        elif opcion == '5':
+            print('')
+            print()
+            break   
+main()
